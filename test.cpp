dc31a7b7805790a4522936a9dc5691e1bf240afc
Raul Torres <raul@americanchip.io>
2023-09-21 12:18:38 +0300

    Add binary and test file

---
+++ test.cpp
@@ -0,0 +1,48 @@
+int main() {
+  for (int i = 0; i < 10; i++) {
+  }
+
+  for (int i = 1; i < 10; i++) {
+  }
+
+  int i = 0;
+  for (int j = 0; i < 10; j++) {
+  }
+
+  for (i = 0; i < 10; i++) {
+  }
+
+  for (; i < 10; i++) {
+  }
+
+  for (; 10 > i; i++) {
+  }
+
+  int j = 0;
+  for (; i < 10; j++) {
+  }
+
+  int n = 10;
+  for (; n < j; j++) {
+  }
+
+  for (; j < n; j++) {
+  }
+
+  for (; j < n; j += 1) {
+  }
+
+  for (; j < n; j = j + 1) {
+  }
+
+  for (j = 0; j < n; j = n + 1) {
+  }
+
+  for (;; j = n + 1) {
+  }
+
+  for (;;) {
+  }
+
+  return 0;
+}
